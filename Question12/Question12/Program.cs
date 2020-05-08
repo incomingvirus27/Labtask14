732454b939d99e4d8a1c491ab617d8f552eff954
Nwosu Yahgozie <gozmegatron@gmail.com>
2020-05-08 09:09:55 +0100

    recommit

--- Question12/Question12/Program.cs
+++ Question12/Question12/Program.cs
@@ -13,30 +13,14 @@ namespace Question12
                 int[] numbers = { 3, 4, -5, 7, -8 };
                 for (int i = 0; i < numbers.Length; i++)
                 {
-
                     if (numbers[i] < 0)
                     {
                         numbers[i] = 0;
-
                     }
                     Console.Write(+numbers[i] + " ");
-                };
+                }
             };
             numbers();
-         /*   int[] numbers = { 3, 4, -5, 7, -8 };
-            for (int i = 0; i < numbers.Length; i++)
-            {
-              
-                if(numbers[i] < 0)
-                {
-                    numbers[i] = 0;
-                    
-                }
-                Console.Write(+ numbers[i] + " ");
-            }
-            */
-           
-
         }
     }
 }
--- QuestionEight/QuestionEight/Program.cs
+++ QuestionEight/QuestionEight/Program.cs
@@ -1,24 +1,29 @@
 ﻿using System;
+using System.Collections;
 
 namespace QuestionEight
 {
+    public delegate void NumArr();
     class Program
     {
         static void Main(string[] args)
-        {             
-                int[] numbers = { 3, 4, -5, 7, -8 };
-                string a = "Codebits"
-                for (int i = 0; i < numbers.Length; i++)
+        {
+            NumArr num = () =>
+            {
+                ArrayList numbers = new ArrayList();
+                numbers.Add(23);
+                numbers.Add(21);
+                numbers.Add(-1);
+                numbers.Add(45);
+                numbers.Add(-6);
+                for (int i = 0; i < numbers.Count; i++)
                 {
-
-                    if (numbers[i] < 0)
-                    {
-                        numbers[i] = a;
-                    }
-                    Console.Write(+numbers[i] + " ");
-                };
-            }
-       
+                    if ((int)numbers[i] < 0)
+                        numbers[i] = "Codebits";
+                    Console.WriteLine(numbers[i]);
+                }               
+            };
+            num();
         }  
     }
 }
