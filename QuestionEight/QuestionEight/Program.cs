33503b4c926292c3034fce2a7af12aaa737438a5
Nwosu Yahgozie <gozmegatron@gmail.com>
2020-05-08 08:42:59 +0100

    commit

--- QuestionEight/QuestionEight/Program.cs
+++ QuestionEight/QuestionEight/Program.cs
@@ -5,8 +5,20 @@ namespace QuestionEight
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        {             
+                int[] numbers = { 3, 4, -5, 7, -8 };
+                string a = "Codebits"
+                for (int i = 0; i < numbers.Length; i++)
+                {
+
+                    if (numbers[i] < 0)
+                    {
+                        numbers[i] = a;
+                    }
+                    Console.Write(+numbers[i] + " ");
+                };
+            }
+       
+        }  
     }
 }
--- QuestionThirteen/QuestionThirteen/Program.cs
+++ QuestionThirteen/QuestionThirteen/Program.cs
@@ -2,11 +2,31 @@
 
 namespace QuestionThirteen
 {
+    public delegate void IntegerDelegate();
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IntegerDelegate ShiftArray = () =>
+            {
+                int[] ArrShift = { 2, 5, 4, 7, 1, 8 };
+                int count = 1;
+                for (int i = 0; i < ArrShift.Length; i++)
+                {
+                    if (count == ArrShift.Length)
+                    {
+                        ArrShift[i] = 0;
+                    }
+                    else
+                    {
+                        ArrShift[i] = ArrShift[i + 1];
+                    }
+
+                    Console.Write(ArrShift[i] + ",");
+                    count++;
+                }
+            };
+            ShiftArray();
         }
     }
 }
